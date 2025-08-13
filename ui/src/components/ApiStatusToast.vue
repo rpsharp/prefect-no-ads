8aa90cf6a74d12a7c18cc71be479026ac1d3b563
rpsharp <ryan@duck.com>
2025-08-12 21:55:11 -0400

    Update ApiStatusToast.vue - strip cloud message

--- ui/src/components/ApiStatusToast.vue
+++ ui/src/components/ApiStatusToast.vue
@@ -1,16 +1,8 @@
 <template>
   <div class="flex flex-col gap-2">
     <PHeading :heading="6">
-      Oops. Something went wrong.
+      API temporarily unavailable, retrying.
     </PHeading>
-    <div>
-      You can use Prefect Cloud for free, anytime.
-    </div>
-    <div>
-      <PButton to="https://app.prefect.cloud/auth/sign-up?utm_source=prefect-oss&utm_medium=toast" variant="default" target="_blank">
-        Try it now
-      </PButton>
-    </div>
   </div>
 </template>
 
